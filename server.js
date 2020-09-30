da777d24c0eb945ff6d6f5ddf26db1a5c790eb06
Kae Benton <sonja.k.benton@gmail.com>
2020-09-30 18:32:03 -0400

    MVP met

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+
 const server = express();
+
+function logger(req, res, next) {
+  let method = req.method;
+  let url = req.url;
+  let current_date = new Date();
+  let status = res.statusCode;
+
+  let log = `[${current_date}] ${method}:${url} ${status}`
+  console.log(log)
+  next();
+}
+
+server.use(express.json());
+server.use(logger)
+server.use('/posts', postRouter);
+server.use('/users', userRouter);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
-
-function logger(req, res, next) {}
 
 module.exports = server;
