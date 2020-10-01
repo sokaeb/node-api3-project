0173c580656b56238bbec6d51e5c6be938595194
Kae Benton <sonja.k.benton@gmail.com>
2020-10-01 14:45:32 -0400

    deploying to heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server');
 
-server.listen(5000, () => {
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
     console.log('Server Running...');
 });
--- server.js
+++ server.js
@@ -23,7 +23,10 @@ server.use('/users', userRouter);
 
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({
+    environment: process.env.NODE_ENV,
+    message: "This is working"
+  });
 });
 
 //custom middleware
